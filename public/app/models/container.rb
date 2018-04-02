16982186efa653e5736445a2c4b18c71ec31fb40
Mark Custer <fordmadox@gmail.com>
2018-04-02 17:04:30 -0400

    more stuff

--- public/app/models/container.rb
+++ public/app/models/container.rb
@@ -4,6 +4,9 @@ class Container < Record
     bits = []
     if @json['type']
       bits << I18n.t("enumerations.container_type.#{@json['type']}", :default => @json['type'].capitalize)
+    else
+      # update "Box" bit to grab from a YML file.
+      bits << 'Box'
     end
     bits << @json['indicator']
 
